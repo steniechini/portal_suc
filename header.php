91b3687fe55d6c9df5b5ab2e83b26259908ff2ea
stephanie <stephaniechini00@gmail.com>
2024-01-04 13:58:30 -0300

    0401

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
         <header id="header">
             <nav class="navbar navbar-expand-lg navbar-light ">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="home.php">
+                    <a class="navbar-brand" href="index.php">
                         <img src="img/logo.webp" alt="logo com as letras S U C estilizadas" >
                     </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
