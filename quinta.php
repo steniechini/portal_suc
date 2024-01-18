d6f6737e1da6527eea212073f1b8073471b54fd4
stephanie <stephaniechini00@gmail.com>
2024-01-18 15:36:16 -0300

    esqueleto da pagina

---
+++ quinta.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- BBootstrap Icons via CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <title>Quinta do Olivardo</title>
+</head>
+<body class="quinta" >
+<?php include("header.php") ?>
+<main>
+   
+    
+    <div class="container">
+                <div class="row"> 
+                    <div class="col-12">
+                            
+                     <h1>Quinta do Olivardo</h1>
+                    </div>
+                </div>
+
+            <div class="row">
+                <div class="col-12">
+                 <section> <img class="img-fluid" src=https://placehold.co/1000x200 alt=""> </section>
+
+                 </div>
+            </div>
+
+            <div class="row">
+                 <div class="col-12">
+                    <article> <P>Lorem ipsum dolor,Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut.
+                     sit amet consectetur adipisicing elit. 
+                     Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut. </P>
+                    </article>
+
+                 </div>
+            </div>
+
+         <div class="row">
+            <div class="col-6">
+                <section> <P>Lorem ipsum dolor,Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut.
+                     sit amet consectetur adipisicing elit. 
+                     Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut. </P>
+                     </section>
+                    
+            </div>
+            <div class="col-6">
+                <section> <img class="img-fluid" src=https://placehold.co/600x400 alt="">  </section>
+                    
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+            <section> <P>Lorem ipsum dolor,Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut.
+                     sit amet consectetur adipisicing elit. 
+                     Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut. </P>
+                     </section>
+                    
+            </div>
+           
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+                <section> <img class="img-fluid" src=https://placehold.co/1200x200 alt=""> </section>
+
+            </div>
+        </div>  
+
+        <div class="row">
+            <div class="col-12">
+            <section> <P>Lorem ipsum dolor,Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut.
+                     sit amet consectetur adipisicing elit. 
+                     Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut. </P>
+                     </section>
+
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+            <section> <img class="img-fluid" src=https://placehold.co/600x400 alt=""> </section>
+
+            </div>
+            <div class="col-4">
+            <section> <img class="img-fluid" src=https://placehold.co/600x400 alt=""> </section>
+
+            </div>
+            <div class="col-4">
+            <section> <img class="img-fluid" src=https://placehold.co/600x400 alt=""> </section>
+
+            </div>
+            
+        </div>
+
+        <div class="row">
+            <div class="col-12">
+            <section> <P>Lorem ipsum dolor,Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut.
+                     sit amet consectetur adipisicing elit. 
+                     Odio at minima blanditiis quod cumque cum consequuntur. Cum, dignissimos provident consequatur cupiditate rem repellendus quibusdam aliquid alias. Sit adipisci cupiditate ut. </P>
+                     </section>
+
+            </div>
+        </div>
+
+
+
+
+
+
+    </div>
+    
+    
+</main>
+<?php include("footer.php") ?>
+</body>
+</html>
