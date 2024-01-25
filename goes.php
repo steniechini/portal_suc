48da3550c3bd41b0960df659d7745186baef93b4
Lucas Gonçalves <lucassaoroque127@gmail.com>
2024-01-25 11:03:27 -0300

    adicionando funçoes

---
+++ goes.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    
+    <!-- BBootstrap Icons via CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="./css/goes.css">
+    
+    <title>Portal S.U.C</title>
+
+</head>
+<body>
+
+<?php include("header.php") ?>
+
+
+<aside class="header-goes">
+    
+<h1 class="">Vinicola Goes <br>1938</h1>
+
+</aside>
+
+
+
+
+<hr>
+<aside class="side-img">
+
+    <img src="https://placehold.co/600x400" alt="">
+    <img src="https://placehold.co/600x400" alt="">
+    <img src="https://placehold.co/600x400" alt="">
+
+</aside>
+
+
+
+<hr>
+
+
+
+
+<main class="main-goes">
+
+
+
+
+
+
+
+<section class="contente-1">
+
+<article class="contente">
+
+    <hgroup class="hgroup-content">
+    <h3 class="h3-content">100% natural</h3>
+    <h1 class="h1-content">Visit & Join Us For A Tasting</h1> 
+    
+    </hgroup>
+
+
+   <section class="bandi">
+   <img class="img-bandi" src="https://placehold.co/100x100" alt="">
+    <div class="heaven-to">
+    <h3 class="h3-bandi">Harvesting</h3>
+    <p class="p-bandi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique commodi cum ab illo animi eveniet fugit itaque impedit blanditiis hic incidunt, porro deleniti doloribus nam id accusantium vero ratione. Quasi!</p>
+    </div>
+   </section>
+
+   <section class="bandi" >
+   <img class="img-bandi" src="https://placehold.co/100x100" alt="">
+    <div class="heaven-to">
+    <h3 class="h3-bandi" >Harvesting</h3>
+    <p class="p-bandi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique commodi cum ab illo animi eveniet fugit itaque impedit blanditiis hic incidunt, porro deleniti doloribus nam id accusantium vero ratione. Quasi!</p>
+    </div>
+   </section>
+
+
+
+   <section class="bandi" >
+   <img class="img-bandi" src="https://placehold.co/100x100" alt="">
+    <div class="heaven-to">
+    <h3 class="h3-bandi" >Harvesting</h3>
+    <p class="p-bandi">Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique commodi cum ab illo animi eveniet fugit itaque impedit blanditiis hic incidunt, porro deleniti doloribus nam id accusantium vero ratione. Quasi!</p>
+    
+   
+
+   
+    </div>
+   </section>
+  
+   <button class="button-bandi">Visit a Adega</button>
+
+
+</article>
+
+<img src="https://placehold.co/400x800" alt="">
+
+</section>
+
+
+
+
+<hr>
+
+
+
+
+
+
+
+
+<section class="vinhos">
+<h3>Novos Vinhos</h3>
+<h1>Recomendações e Vinhos</h1>
+
+<div class="gridando">
+<img src="https://placehold.co/200x300" alt="">
+<img src="https://placehold.co/200x300" alt="">
+<img src="https://placehold.co/200x300" alt="">
+<img src="https://placehold.co/200x300" alt="">
+<img src="https://placehold.co/200x300" alt="">
+<img src="https://placehold.co/200x300" alt="">
+</div>
+
+
+
+
+</section>
+
+<hr>
+<section>
+
+<article class="safra">
+
+    <img src="https://placehold.co/600x400" alt="">
+
+    <div class="safrinha">
+
+    <hgroup>
+    <h3 class="h3-content">Experience</h3>
+    <h1 class="h1-content">The Story Behind <br>Our Wine</h1>
+    </hgroup>
+
+    <article class="article-safra">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat cumque molestiae cupiditate delectus minima quam. Aspernatur, consequuntur expedita! Id nostrum quidem voluptatibus temporibus dolorum earum omnis impedit nisi a fugiat?</p>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ipsum qui ipsa a, hic ea unde tenetur non consequatur tempora laborum nulla temporibus autem perferendis numquam eum esse dolor nobis.</p>
+
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque ipsum qui ipsa a, hic ea unde tenetur non consequatur tempora laborum nulla temporibus autem perferendis numquam eum esse dolor nobis.</p>
+    </article>
+
+
+    <button class="button-bandi">Saiba Mais -></button>
+        
+    </div>
+</article>
+</section>
+
+<hr>
+
+<section class="sacando">
+
+<h1>Entenda a Safra</h1>
+
+<img src="https://placehold.co/600x800" alt="">
+
+
+<!-- monte de coisas conectando com a imagem e explicando -->
+
+
+</section>
+
+
+<hr>
+
+
+
+
+
+
+<section class="nova-linha ">
+
+<article class="article-d">
+    <h1>Nova Linha</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor vitae recusandae maxime illo facere ut dolores provident a repudiandae sint excepturi perspiciatis consectetur tempora aperiam modi incidunt, alias pariatur! Repellat!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, voluptatum. Quaerat, eligendi sapiente dolores, eveniet perferendis beatae atque distinctio repellendus, consequatur et quidem sit quasi rem nisi impedit reiciendis nesciunt!</p>
+</article>
+
+<img src="https://placehold.co/600x800" alt="">
+
+<article>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor vitae recusandae maxime illo facere ut dolores provident a repudiandae sint excepturi perspiciatis consectetur tempora aperiam modi incidunt, alias pariatur! Repellat!</p>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, voluptatum. Quaerat, eligendi sapiente dolores, eveniet perferendis beatae atque distinctio repellendus, consequatur et quidem sit quasi rem nisi impedit reiciendis nesciunt!</p>
+</article>
+
+
+</section>
+<button>Conhea Mais</button>
+
+<hr>
+
+<section class="feedback">
+
+    <h1>FeedBack dos Clientes</h1>
+
+    <img src="https://placehold.co/600x800" alt="">
+    <img src="https://placehold.co/600x800" alt="">
+    <img src="https://placehold.co/600x800" alt="">
+    
+</section>
+
+<hr>
+
+<section class="oferta">
+<h1>Oferta</h1>
+<img src="https://placehold.co/600x800" alt="">
+
+
+</section>
+
+<hr>
+
+<section>
+
+    <h1>Conheça a Familia Goes</h1>
+
+    <img src="https://placehold.co/600x800" alt="">
+    <img src="https://placehold.co/600x800" alt="">
+    <img src="https://placehold.co/600x800" alt="">
+    
+</section>
+</main>
+
+<footer>
+
+</footer>
+
+
+<footer>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#00cba9" fill-opacity="1" d="M0,64L34.3,80C68.6,96,137,128,206,128C274.3,128,343,96,411,85.3C480,75,549,85,617,122.7C685.7,160,754,224,823,224C891.4,224,960,160,1029,117.3C1097.1,75,1166,53,1234,58.7C1302.9,64,1371,96,1406,112L1440,128L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"></path></svg>
+</footer>
+
+
+
+<?php include("footer.php") ?>
+
+
+</body>
+</html>
