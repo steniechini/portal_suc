b6c1f86080339e27f560df535cc10e85bdbdbbbd
thaminf <thami.tiyoko@gmail.com>
2024-02-14 22:29:52 -0300

    finalizando página vila don patto

--- donpatto.php
+++ donpatto.php
@@ -17,22 +17,42 @@
 <body class="villadonpatto">
     <?php include("header.php") ?>
     <main class="conteudo">
-    <section class="conteudo-principal">
-      <div class="conteudo-principal-escrito">
-        <h1 class="conteudo-principal-titulo">Vila Don Patto</h1>
-        <h2 class="conteudo-principal-subtitulo">Conheça um dos restaurentes mais famosos do Roteiro do Vinho!</h2>
-        <h3 class="conteudo-principal-texto">A Vila don Patto é o maior complexo gastronômico de São Roque. Uma área verde com uma espetacular vista para as montanhas, a menos de uma hora de São Paulo. A Vila possui diversos ambientes. </h3>
-        <button class="conteudo-principal-botao" href="#conteudo-secundario">Saber mais</button>
-      </div>
-      <img class="conteudo-principal-imagem" src="img/donpatto/plate-vegetable-stew-served-with-white-wine-bread-_1_.webp" alt="imagem de comida">
-    </section>
+        <section class="conteudo-principal">
+            <div class="conteudo-principal-escrito">
+                <h1 class="conteudo-principal-titulo">Vila Don Patto</h1>
+                <h2 class="conteudo-principal-subtitulo">Conheça um dos restaurentes mais famosos do Roteiro do Vinho!</h2>
+                <h3 class="conteudo-principal-texto">A Vila don Patto é o maior complexo gastronômico de São Roque. Uma área verde com uma espetacular vista para as montanhas, a menos de uma hora de São Paulo. A Vila possui diversos ambientes. </h3>
+                <button class="conteudo-principal-botao" onclick="window.location.href='#conteudo-secundario'">Saber mais</button>
+            </div>
+            <img class="conteudo-principal-imagem" src="img/donpatto/plate-vegetable-stew-served-with-white-wine-bread-_1_.webp" alt="imagem de comida">
+        </section>
 
-    <hr>
+        <section class="conteudo-secundario" id="conteudo-secundario">
+            <h1 class="conteudo-secundario-titulo">Conheça as culinárias</h1>
+            <img class="conteudo-secundario-imagem-portuguesa" src="img/donpatto/portuguesa.png" alt="prato de comida portuguesa">
+            <h1 class="conteudo-secundario-subtitulo">Culinária Portuguesa</h1>
+            <p class="conteudo-secundario-texto">A culinária portuguesa é uma fusão de sabores e influências, refletindo a rica história do país e sua proximidade com o mar. Com uma forte ênfase nos frutos do mar frescos, a comida portuguesa é conhecida por seus pratos deliciosos e reconfortantes. Desde os famosos pratos de bacalhau, como o bacalhau à Brás e o bacalhau à Gomes de Sá, até as saborosas sopas, como a caldeirada e a sopa de peixe, a culinária portuguesa é uma celebração da abundância do mar e da terra. Os sabores intensos dos azeites, dos temperos e das ervas dão vida aos pratos, criando uma experiência gastronômica única e inesquecível.</p>
+            <img class="conteudo-secundario-imagem-italiana" src="img/donpatto/comidas-tipicas-da-italia.jpg" alt="prato de comida portuguesa">
+            <h1 class="conteudo-secundario-subtitulo">Culinária Italiana</h1>
+            <p class="conteudo-secundario-texto">A culinária italiana é uma das mais influentes e amadas em todo o mundo, conhecida por sua simplicidade, ingredientes frescos e sabores robustos. Com uma rica variedade de pratos regionais, a comida italiana é uma celebração da diversidade de suas regiões. Desde as tradicionais massas como o espaguete e a lasanha, até os deliciosos risotos e pizzas, cada prato é uma verdadeira obra-prima que reflete a paixão e a tradição culinária do país. Os ingredientes frescos, como o azeite de oliva, o tomate, as ervas aromáticas e os queijos, são a base da cozinha italiana, resultando em refeições que são simplesmente irresistíveis.</p>
+        </section>
 
-    <section class="conteudo-secundario">
-      
-    </section>
-  </main>
+        <section class="galeria-de-fotos">
+            <h1 class="conteudo-secundario-titulo">Conheça nosso espaço</h1>
+            <div class="galeria-container">
+                <div class="imagem"><img src="img/donpatto/adega.webp" alt="Foto 1"></div>
+                <div class="imagem"><img src="img/donpatto/guarda-chuvas.jpg" alt="Foto 2"></div>
+                <div class="imagem"><img src="img/donpatto/rede.jpg" alt="Foto 3"></div>
+                <div class="imagem"><img src="img/donpatto/restaurante1.jpg" alt="Foto 4"></div>
+                <div class="imagem"><img src="img/donpatto/restaurante2.jpg" alt="Foto 5"></div>
+                <div class="imagem"><img src="img/donpatto/casinha.jpg" alt="Foto 6"></div>
+                <div class="imagem"><img src="img/donpatto/paisagem.jpg" alt="Foto 7"></div>
+                <div class="imagem"><img src="img/donpatto/lounge.jpg" alt="Foto 8"></div>
+                <div class="imagem"><img src="img/donpatto/lago.jpg" alt="Foto 9"></div>
+            </div>
+
+        </section>
+    </main>
     <?php include("footer.php") ?>
 </body>
 
