eafb73494596fc4a31585a6b332f3c2f4fe1e166
thaminf <thami.tiyoko@gmail.com>
2024-02-14 21:20:15 -0300

    primeira parte desenvolvida do don patto

--- donpatto.php
+++ donpatto.php
@@ -8,22 +8,29 @@
     <!-- Bootstrap CSS v5.2.1 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/donpatto.css">
     <!-- BBootstrap Icons via CDN -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-    <title>Culinária Portuguesa</title>
+    <title>Vila Don Patto</title>
 </head>
 
-<body class="portuguesa">
+<body class="villadonpatto">
     <?php include("header.php") ?>
     <main class="conteudo">
     <section class="conteudo-principal">
       <div class="conteudo-principal-escrito">
         <h1 class="conteudo-principal-titulo">Vila Don Patto</h1>
         <h2 class="conteudo-principal-subtitulo">Conheça um dos restaurentes mais famosos do Roteiro do Vinho!</h2>
-        <button class="conteudo-principal-botao">Saber mais</button>
+        <h3 class="conteudo-principal-texto">A Vila don Patto é o maior complexo gastronômico de São Roque. Uma área verde com uma espetacular vista para as montanhas, a menos de uma hora de São Paulo. A Vila possui diversos ambientes. </h3>
+        <button class="conteudo-principal-botao" href="#conteudo-secundario">Saber mais</button>
       </div>
-      <img class="conteudo-principal-imagem"src="" alt="imagem de comida">
+      <img class="conteudo-principal-imagem" src="img/donpatto/plate-vegetable-stew-served-with-white-wine-bread-_1_.webp" alt="imagem de comida">
+    </section>
+
+    <hr>
+
+    <section class="conteudo-secundario">
+      
     </section>
   </main>
     <?php include("footer.php") ?>
